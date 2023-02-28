670001c5868e6af603e992e15f86d42bf1737695
alumno <alumno@30010577F138B8>
2023-02-28 14:37:33 +0100

    Creacion de clase 1

--- GitHub2/src/main/java/com/mycompany/github2/Clase1.java
+++ GitHub2/src/main/java/com/mycompany/github2/Clase1.java
@@ -3,5 +3,5 @@ package com.mycompany.github2;
 
 
 public class Clase1 {
-    
+    private int at1;
 }
--- GitHub2/src/main/java/com/mycompany/github2/NewMain.java
+++ GitHub2/src/main/java/com/mycompany/github2/NewMain.java
@@ -7,8 +7,10 @@ public class NewMain {
     
     public static void main(String[] args) {
        
-        
+        System.out.println("asdf");
+        System.out.println("asdfg");
         System.out.println("mensaje 1");
+        System.out.println("dsfhdsa");
     }
     
     
